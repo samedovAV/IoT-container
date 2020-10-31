ad55a663d059662d74daba7a610e5ad9a95ac627
samedovAV <alekcandr206@gmail.com>
2020-10-31 20:21:43 +0300

    fix packages

--- src/main/java/generators/XMLGenerator.java
+++ src/main/java/generators/XMLGenerator.java
@@ -1,6 +1,5 @@
 package generators;
 
-import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -8,7 +7,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
--- src/main/java/parsers/HTMLParser.java
+++ src/main/java/parsers/HTMLParser.java
@@ -6,20 +6,14 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class HTMLParser {
 
-    public static void main(String[] args) throws IOException, ParserConfigurationException {
+    public static void main(String[] args) throws IOException {
         parseHTML();
     }
 
-    public static void parseHTML() throws IOException, ParserConfigurationException {
+    public static void parseHTML() throws IOException {
         File input = new File("dataFromContainer.html");
 
         FileWriter output = new FileWriter("dataFromContainer(HTML).txt");
